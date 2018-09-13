acc9bfb6cae27abcaee0a09afb1162ec9f6638de
ZWJ <330564052@qq.com>
2018-09-13 23:30:15 +0800

    * 初次提交

---
+++ AdjustDevice/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace AdjustDevice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "扫描串口";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(220, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "打开串口";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(301, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "关闭串口";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column7,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 25;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(623, 229);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(560, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "保存配置";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column1.HeaderText = "序号";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column1.Width = 55;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "遥测名称";
+            this.Column2.Name = "Column2";
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "遥测值";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "倍率";
+            this.Column4.Items.AddRange(new object[] {
+            "0.001",
+            "0.01",
+            "0.1"});
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 80;
+            // 
+            // Column7
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Column7.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column7.HeaderText = "单位";
+            this.Column7.Items.AddRange(new object[] {
+            "A",
+            "V",
+            "W",
+            "Var",
+            "VA",
+            "℃",
+            "%RH"});
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 80;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "符号";
+            this.Column5.Items.AddRange(new object[] {
+            "无符号",
+            "有符号"});
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 80;
+            // 
+            // Column6
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Column6.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column6.HeaderText = "备注";
+            this.Column6.Name = "Column6";
+            this.Column6.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 324);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AdjustDevice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
---
+++ AdjustDevice/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace AdjustDevice
+{
+
+    public partial class Form1 : Form
+    {
+        public string path = Environment.CurrentDirectory + @"\Config.ini";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SearchAndAddSerialToComboBox(serialPort1, comboBox1);
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Rows.Add(9);
+            IniFile ini = new IniFile(path);
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                row.Cells[0].Value = row.Index + 1;
+            }
+
+            if (ini.ExistINIFile(path))
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    row.Cells[1].Value = ini.IniReadValue("name", "name" + row.Index.ToString());
+                    row.Cells[3].Value = ini.IniReadValue("Magnification", "Mag" + row.Index.ToString());
+                    row.Cells[4].Value = ini.IniReadValue("unit", "unit" + row.Index.ToString());
+                    row.Cells[5].Value = ini.IniReadValue("symbol", "symbol" + row.Index.ToString());
+                    row.Cells[6].Value = ini.IniReadValue("Remark", "Remark" + row.Index.ToString());
+                }
+            }
+
+        }
+
+        private void SearchAndAddSerialToComboBox(SerialPort MyPort, ComboBox MyBox)
+        {                                                               //将可用端口号添加到ComboBox  
+            string Buffer;                                              //缓存  
+            MyBox.Items.Clear();                                        //清空ComboBox内容  
+            for (int i = 1; i < 20; i++)                                //循环这里只扫描1-19  
+            {
+                try                                                     //核心原理是依靠try和catch完成遍历  
+                {
+                    Buffer = "COM" + i.ToString();
+                    MyPort.PortName = Buffer;
+                    MyPort.Open();                                      //如果失败，后面的代码不会执行                                                                          
+                    MyBox.Items.Add(Buffer);                            //打开成功，添加至下俩列表  
+                    MyPort.Close();                                     //关闭  
+                    MyBox.Text = Buffer;
+                }
+                catch//出错了什么也不做继续循环  
+                {
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBox1.Text;
+            serialPort1.Open();
+            button2.Enabled = false;
+            if (!button3.Enabled)
+            {
+                button3.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            button3.Enabled = false;
+            if (button2.Enabled)
+            {
+                button3.Enabled = false;
+            }
+            else
+            {
+                button2.Enabled = true;
+            }
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            IniFile ini = new IniFile(path);
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                ini.IniWriteValue("name", "name" + row.Index.ToString(), row.Cells[1].Value.ToString());
+                ini.IniWriteValue("Magnification", "Mag" + row.Index.ToString(), row.Cells[3].Value.ToString());
+                ini.IniWriteValue("unit", "unit" + row.Index.ToString(), row.Cells[4].Value.ToString());
+                ini.IniWriteValue("symbol", "symbol" + row.Index.ToString(), row.Cells[5].Value.ToString());
+                ini.IniWriteValue("Remark", "Remark" + row.Index.ToString(), row.Cells[6].Value.ToString());
+            }
+            MessageBox.Show("保存配置成功");
+            FileInfo info = new FileInfo(path);
+            if (info.Exists)
+            {
+                info.Attributes = FileAttributes.Hidden;
+            }
+        }
+
+
+        public class IniFile
+        {
+            public string Path;
+
+            public IniFile(string path)
+            {
+                this.Path = path;
+            }
+
+            #region 声明读写INI文件的API函数
+
+            [DllImport("kernel32")]
+            private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+            [DllImport("kernel32")]
+            private static extern int GetPrivateProfileString(string section, string key, string defVal, StringBuilder retVal, int size, string filePath);
+
+            [DllImport("kernel32")]
+            private static extern int GetPrivateProfileString(string section, string key, string defVal, Byte[] retVal, int size, string filePath);
+
+            #endregion
+
+            /// <summary>
+            /// 写INI文件
+            /// </summary>
+            /// <param name="section">段落</param>
+            /// <param name="key">键</param>
+            /// <param name="iValue">值</param>
+            public void IniWriteValue(string section, string key, string iValue)
+            {
+                WritePrivateProfileString(section, key, iValue, this.Path);
+            }
+
+            /// <summary>
+            /// 读取INI文件
+            /// </summary>
+            /// <param name="section">段落</param>
+            /// <param name="key">键</param>
+            /// <returns>返回的键值</returns>
+            public string IniReadValue(string section, string key)
+            {
+                StringBuilder temp = new StringBuilder(255);
+
+                int i = GetPrivateProfileString(section, key, "", temp, 255, this.Path);
+                return temp.ToString();
+            }
+
+            /// <summary>
+            /// 读取INI文件
+            /// </summary>
+            /// <param name="Section">段，格式[]</param>
+            /// <param name="Key">键</param>
+            /// <returns>返回byte类型的section组或键值组</returns>
+            public byte[] IniReadValues(string section, string key)
+            {
+                byte[] temp = new byte[255];
+
+                int i = GetPrivateProfileString(section, key, "", temp, 255, this.Path);
+                return temp;
+            }
+
+            public bool ExistINIFile(string path)
+            {
+                return File.Exists(path);
+            }
+        }
+
+
+    }
+}
